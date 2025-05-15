9dc6c622fc3948086e866e47ab650ff948d67c71
uncle wang <v1v9a1b2@qq.com>
2025-05-15 06:10:13 +0000

    Initial commit with generated client

---
+++ README.md
@@ -0,0 +1,32 @@
+# zsgf-client@1.0.25
+
+![NPM Version](https://img.shields.io/npm/v/zsgf-client)
+
+基于 [Fetch API](https://fetch.spec.whatwg.org/) 的 TypeScript/JavaScript 客户端。
+
+## 运行环境
+
+* Node.js
+* Webpack
+* Browserify
+
+## 语言
+
+* ES5 - 需要安装一个 Promises/A+ 库
+* ES6
+
+## 模块系统
+
+* CommonJS
+* ES6 模块系统
+
+
+同时用于 TypeScript 和 JavaScript。在 TypeScript 中，类型定义会通过 package.json 自动解析。([参考](https://www.typescriptlang.org/docs/handbook/declaration-files/consumption.html))
+
+## 使用方式
+
+进入项目，运行以下命令
+
+```
+npm install zsgf-client@1.0.25 --save
+```
---
+++ src/apis/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccessTokenApi';
+export * from './AlipayApi';
+export * from './AppApi';
+export * from './AppSettingApi';
+export * from './AuthorizePolicyApi';
+export * from './CurrencyApi';
+export * from './DingTalkApi';
+export * from './FileApi';
+export * from './OAuthApi';
+export * from './OrderApi';
+export * from './ProjectApi';
+export * from './ServiceSettingApi';
+export * from './StorageApi';
+export * from './SystemFileApi';
+export * from './TeamApi';
+export * from './UserApi';
+export * from './UserCurrencyApi';
+export * from './UserSettingApi';
+export * from './WechatApi';
