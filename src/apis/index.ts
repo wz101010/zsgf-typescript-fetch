a2139d7f0e0f56e8027b4021d21008a242869ac9
uncle wang <v1v9a1b2@qq.com>
2026-06-03 06:09:58 +0000

    Initial commit with generated client

---
+++ src/apis/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccessTokenApi';
+export * from './AlipayApi';
+export * from './AppApi';
+export * from './DingTalkApi';
+export * from './ExternalAccountApi';
+export * from './FileApi';
+export * from './OAuthApi';
+export * from './OrderApi';
+export * from './StorageApi';
+export * from './UserApi';
+export * from './UserCurrencyApi';
+export * from './UserFriendsApi';
+export * from './UserLocationApi';
+export * from './WechatApi';
